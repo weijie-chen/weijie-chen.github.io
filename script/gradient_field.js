21bd54fd941cbdd3b2ba6cff1b582e6e36c80ecd
weijie-chen <weijiechen00@gmail.com>
2024-07-31 16:55:45 +0200

    all use cdn import

--- script/gradient_field.js
+++ script/gradient_field.js
@@ -1,6 +1,7 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.167.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@v0.167.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@v0.167.0/examples/jsm/loaders/GLTFLoader.js';
+import * as BufferGeometryUtils from 'https://cdn.jsdelivr.net/npm/three@v0.167.0/examples/jsm/utils/BufferGeometryUtils.js';
         
 
 function initThreeJSAnimation(containerId) {
