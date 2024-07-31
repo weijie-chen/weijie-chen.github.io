5fb679f81e114ecf338e6ccc8a64b9423a21fa4f
weijie-chen <weijiechen00@gmail.com>
2024-07-31 11:53:48 +0200

    version

--- script/gradient_field.js
+++ script/gradient_field.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.167.0";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.167.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://cdn.skypack.dev/three@0.150.0";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.150.0/examples/jsm/controls/OrbitControls.js";
 import * as BufferGeometryUtils from "https://cdn.skypack.dev/three@0.167.0/examples/jsm/utils/BufferGeometryUtils.js";
 
 function initThreeJSAnimation(containerId) {
